be801cf22bc89821ca8bff2af08a850def8b42ca
jim_kim_ <75765858+jim-kim-playground@users.noreply.github.com>
2021-09-08 17:39:05 +0900

    Updated TODO

--- pages/exchange-rates.tsx
+++ pages/exchange-rates.tsx
@@ -5,6 +5,9 @@ import ExchangeRatesService from '../src/services/exchange-rates';
 // TODO: use material-ui classes, layout.. not Home.modul.css ...
 // https://material-ui.com/components/drawers/#mini-variant-drawer
 
+// TODO: draw graph
+// https://devexpress.github.io/devextreme-reactive/react/chart/demos/line/line/
+
 const ExchangeRates: FC = () => {
   return <div />;
 };
