24d3d0926e93b5edbc4a1b98e6014d6c4db9a0a7
jim_kim_ <75765858+jim-kim-playground@users.noreply.github.com>
2021-09-08 17:26:26 +0900

    Build chart in the Server, SSR

--- pages/exchange-rates.tsx
+++ pages/exchange-rates.tsx
@@ -1,16 +1,15 @@
 import { FC } from 'react';
-import { Button } from '@material-ui/core';
 import { GetServerSideProps } from 'next';
 import ExchangeRatesService from '../src/services/exchange-rates';
-
-interface SupProps {
-  message: string;
-}
 
 // TODO: use material-ui classes, layout.. not Home.modul.css ...
 // https://material-ui.com/components/drawers/#mini-variant-drawer
 
-const onClickHandler = () => {
+const ExchangeRates: FC = () => {
+  return <div />;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
   try {
     ExchangeRatesService.getExchangeRate().then((response) => {
       console.log(response);
@@ -18,20 +17,7 @@ const onClickHandler = () => {
   } catch (error) {
     console.log(error);
   }
-};
 
-const ExchangeRates: FC<SupProps> = ({ message }: SupProps) => {
-  return (
-    <div>
-      <Button color="primary" variant="contained" onClick={onClickHandler}>
-        {message}
-      </Button>
-    </div>
-  );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  // ExchangeRatesService.getExchangeRate();
   return {
     props: {
       message: 'sup',
