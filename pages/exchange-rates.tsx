f2a9001dfa4ef835a3b46dfd2dd47344703be27b
jim_kim_ <75765858+jim-kim-playground@users.noreply.github.com>
2021-10-01 18:23:00 +0900

    Tring to use vega-lite to draw graph

--- pages/exchange-rates.tsx
+++ pages/exchange-rates.tsx
@@ -1,19 +1,30 @@
 import { FC } from 'react';
 import { GetServerSideProps } from 'next';
+// import vega from 'vega';
+// import vegaLite from 'vega-lite';
+// import vl from 'vega-lite-api';
 import { ExchangeRateResponseModel, Quote } from '@src/models/exchange-rates';
 import ExchangeRatesService from '@src/services/exchange-rates';
 
 // TODO: use material-ui classes, layout.. not Home.modul.css ...
 // https://material-ui.com/components/drawers/#mini-variant-drawer
 
-// TODO: draw graph
-// https://devexpress.github.io/devextreme-reactive/react/chart/demos/line/line/
+// TODO: Draw graph
+// https://vizhub.com/curran/7392ee124c8249b884e735abf6da8df4?edit=files&file=index.js
+
+// vl.register(vega, vegaLite, {
+//   view: { renderer: 'svg' },
+//   init: view => { view.tooltip(new Handler().call); }
+// });
 
 const getValues = (values: Array<Quote>) => {
   return (
     <div>
       {values.map((value: Quote) => (
-        <div>{value.close}</div>
+        <div>
+          {value.date}
+          {value.close}
+        </div>
       ))}
     </div>
   );
