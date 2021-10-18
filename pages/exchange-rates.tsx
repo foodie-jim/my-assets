9d194d51dd818cedbfc5dce3ae5e8f3fee1e6201
jim_kim_ <75765858+jim-kim-playground@users.noreply.github.com>
2021-10-18 16:10:13 +0900

    Added bar chart, TBD

--- pages/exchange-rates.tsx
+++ pages/exchange-rates.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react';
 import { GetServerSideProps } from 'next';
-// import vega from 'vega';
-// import vegaLite from 'vega-lite';
-// import vl from 'vega-lite-api';
-import { ExchangeRateResponseModel, Quote } from '@src/models/exchange-rates';
-import ExchangeRatesService from '@src/services/exchange-rates';
+import { Vega } from 'react-vega';
+import { Config, TopLevelSpec, compile } from 'vega-lite';
+// import { ExchangeRateResponseModel, Quote } from '@src/models/exchange-rates';
+// import ExchangeRatesService from '@src/services/exchange-rates';
 
 // TODO: use material-ui classes, layout.. not Home.modul.css ...
 // https://material-ui.com/components/drawers/#mini-variant-drawer
@@ -12,38 +11,73 @@ import ExchangeRatesService from '@src/services/exchange-rates';
 // TODO: Draw graph
 // https://vizhub.com/curran/7392ee124c8249b884e735abf6da8df4?edit=files&file=index.js
 
-// vl.register(vega, vegaLite, {
-//   view: { renderer: 'svg' },
-//   init: view => { view.tooltip(new Handler().call); }
-// });
+// const getValues = (values: Array<Quote>) => {
+//   return (
+//     <div>
+//       {values.map((value: Quote) => (
+//         <div>
+//           {value.date}
+//           {value.close}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-const getValues = (values: Array<Quote>) => {
+// const ExchangeRates: FC<ExchangeRateResponseModel> = ({ dollarIndex, exchangeRates }: ExchangeRateResponseModel) => {
+//   return (
+//     <div>
+//       <div>{getValues(dollarIndex)}</div>
+//       <div>{getValues(exchangeRates)}</div>
+//     </div>
+//   );
+// };
+
+// const ExchangeRates: FC<ExchangeRateResponseModel> = ({ dollarIndex, exchangeRates }: ExchangeRateResponseModel) => {
+// eslint-disable-next-line react/prop-types
+const ExchangeRates: FC = (vegaSpec) => {
   return (
     <div>
-      {values.map((value: Quote) => (
-        <div>
-          {value.date}
-          {value.close}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const ExchangeRates: FC<ExchangeRateResponseModel> = ({ dollarIndex, exchangeRates }: ExchangeRateResponseModel) => {
-  return (
-    <div>
-      <div>{getValues(dollarIndex)}</div>
-      <div>{getValues(exchangeRates)}</div>
+      <Vega spec={vegaSpec} actions={false} renderer="svg" />
     </div>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await ExchangeRatesService.getExchangeRate();
+  // const response = await ExchangeRatesService.getExchangeRate();
+
+  const vegaLiteSpec: TopLevelSpec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    data: {
+      values: [
+        { a: 'A', b: 28 },
+        { a: 'B', b: 55 },
+        { a: 'C', b: 43 },
+        { a: 'D', b: 91 },
+        { a: 'E', b: 81 },
+        { a: 'F', b: 53 },
+        { a: 'G', b: 19 },
+        { a: 'H', b: 87 },
+        { a: 'I', b: 52 },
+      ],
+    },
+    mark: 'bar',
+    encoding: {
+      x: { field: 'a', type: 'nominal', axis: { labelAngle: 0 } },
+      y: { field: 'b', type: 'quantitative' },
+    },
+  };
+
+  const config: Config = {
+    bar: {
+      color: 'firebrick',
+    },
+  };
+
+  const vegaSpec = compile(vegaLiteSpec, { config }).spec;
 
   return {
-    props: response,
+    props: vegaSpec,
   };
 };
 
